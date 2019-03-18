b69ae5e67d6e235f83413b3af963078e89077dd4
Yuri Araújo <araujoyuri@edu.unifor.br>
2019-03-18 13:29:11 -0300

    Added option for cli

--- src/parser.py
+++ src/parser.py
@@ -1,25 +1,39 @@
 import argparse
 import re
 
-
 patterns = [
-	r'(mov)\s+(\w+),\s+(\d+)',
-	r'(mov)\s+(0x\d+),\s+(\w+)',
-	r'(add)\s+(\w+),\s+(\w+)',
-	r'(inc)\s+(0x\d+)',
-	r'(imul)\s+(\w+),\s+(0x\d+),\s+(\d+)'
+    r'(mov)\s*(\w+),\s*(\d+)',
+    r'(mov)\s*(0x\d+),\s*(\w+)',
+    r'(add)\s*(\w+),\s*(\w+)',
+    r'(inc)\s*(0x\d+)',
+    r'(imul)\s*(\w+),\s*(0x\d+),\s+(\d+)'
 ]
 
 parser = argparse.ArgumentParser(description='Lê comandos assembly de arquivo')
-parser.add_argument('--path', help='Recebe caminho absoluto para o arquivo de texto', type=argparse.FileType('r'))
+parser.add_argument('--path',
+                    help='Recebe caminho absoluto para o arquivo de texto',
+                    type=argparse.FileType('r'))
+parser.add_argument('--command',
+                    help='Recebe comando direto do terminal')
 
 args = parser.parse_args()
 
-for line in args.path.readlines():
-	for pattern in patterns:
-		matched = re.match(pattern, line)
-		if matched:
-			btArr = bytearray(str(matched.groups()), encoding='utf-8')
-			print('encoded: ', btArr)
-			print('decoded: ', btArr.decode('utf-8', errors='ignore'))
-		print(matched.groups() if matched else None)
+print(args.command)
+
+if args.command:
+    for pattern in patterns:
+        matched = re.match(pattern, args.command)
+        if matched:
+            btArr = bytearray(str(matched.groups()), encoding='utf-8')
+            print('encoded: ', btArr)
+            print('decoded: ', btArr.decode('utf-8', errors='ignore'))
+
+elif args.path:
+    for line in args.path.readlines():
+        for pattern in patterns:
+            matched = re.match(pattern, line)
+            if matched:
+                btArr = bytearray(str(matched.groups()), encoding='utf-8')
+                print('encoded: ', btArr)
+                print('decoded: ', btArr.decode('utf-8', errors='ignore'))
+            print(matched.groups() if matched else None)
